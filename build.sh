a262b1bad06a5bf1a44bfc6c6b0280329e1f5339
Dhvanit2023 <153352630+Dhvanit2023@users.noreply.github.com>
2026-03-06 10:16:16 +0530

    Update build script for Microsoft ODBC installation

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
+set -e
 
 apt-get update
 
-apt-get install -y curl gnupg
+apt-get install -y curl gnupg unixodbc unixodbc-dev
 
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg
 
-curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/ubuntu/22.04/prod jammy main" > /etc/apt/sources.list.d/mssql-release.list
 
 apt-get update
 
-ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev
+ACCEPT_EULA=Y apt-get install -y msodbcsql18
